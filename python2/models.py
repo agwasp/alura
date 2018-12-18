86c7f484b1fc269cc57ea665c4660441b0596837
Alex Giannotti <f1970@brasilprev.com.br>
2018-12-18 16:41:21 -0200

    add classes pessoa e data

--- python2/models.py
+++ python2/models.py
@@ -11,4 +11,22 @@ class Perfil(object):
     
     def imprimir(self):
         print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
-        
+
+class Data(object):
+   def __init__(self, dia, mes, ano):
+      self.dia = dia
+      self.mes = mes
+      self.ano = ano
+
+   def imprimir(self):
+      print '%s/%s/%s' % (self.dia, self.mes, self.ano)
+
+class IMCPessoa(object):
+    def __init__(self, nome, peso, altura):
+        self.nome = nome
+        self.peso = float(peso)
+        self.altura = float(altura)
+
+    def imprimir(self):
+        IMC = (self.altura **2) / self.peso
+        print 'O IMC de %s é: %s' % (self.nome, IMC)
