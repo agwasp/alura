019eb2642c7d143e3a4a5bf9378cfab314a5807a
Alex Giannotti <f1970@brasilprev.com.br>
2018-12-24 10:10:54 -0200

    mais heranca - aula 10

--- python2/models2.py
+++ python2/models2.py
@@ -16,23 +16,12 @@ class Perfil(object):
     def imprimir(self):
         print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
 
-class Perfil_Vip(object):
-    'Classe para padrão de perfis de usuários'
+class Perfil_Vip(Perfil):
+    'Classe para padrão de perfis de usuários VIPs'
 
-    def __init__(self, nome, telefone, empresa):
-        self.nome = nome
-        self.telefone = telefone
-        self.empresa = empresa
-        self.__curtidas = 0
-    
-    def curtir(self):
-        self.__curtidas+=1
-
-    def obter_curtidas(self):
-        return self.__curtidas
+    def __init__(self, nome, telefone, empresa, apelido):
+        super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+        self.apelido = apelido
 
     def obter_creditos(self):
-        return self.__curtidas * 10.0
-
-    def imprimir(self):
-        print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+        return super(Perfil_Vip, self).obter_curtidas() * 10.0
