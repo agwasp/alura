3c1cadb039a07b78e348210873e87b7e46b8108f
Alex Giannotti <f1970@brasilprev.com.br>
2018-12-24 11:12:33 -0200

    add arquivos alua 10 atualizados

--- python2/arquivos aula/Aula 10/models.py
+++ python2/arquivos aula/Aula 10/models.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 class Perfil(object):
     'Classe padrão para perfis de usuários'
@@ -9,11 +9,36 @@ class Perfil(object):
         self.empresa = empresa
         self.__curtidas = 0
 
-    def imprimir(self):
-        print "Nome : %s, Telefone: %s, Empresa %s" % (self.nome, self.telefone, self.empresa)
+        def imprimir(self):
+            print "Nome : %s, Telefone: %s, Empresa: %s, Curtidas: %s" % (self.nome, self.telefone, self.empresa, self.__curtidas)   
 
-    def curtir(self):
-        self.__curtidas+=1
+        def curtir(self):
+            self.__curtidas+=1
 
-    def obter_curtidas(self):
-        return self.__curtidas
+        def obter_curtidas(self):
+            return self.__curtidas
+
+class Perfil_Vip(Perfil):
+   'Classe padrão para perfis de usuários VIPs'
+
+   def obter_creditos(self):
+      return super(Perfil_Vip, self).obter_curtidas() * 10.0
+
+class Data(object):
+   def __init__(self, dia, mes, ano):
+      self.dia = dia
+      self.mes = mes
+      self.ano = ano
+
+   def imprime(self):
+      print '%s/%s/%s' % (self.dia, self.mes, self.ano)
+
+class Pessoa(object):
+    def __init__(self, nome, peso, altura):
+        self.nome = nome
+        self.peso = float(peso)
+        self.altura = float(altura)
+
+    def imprime(self):
+        imc = self.peso / (self.altura **2)
+        print 'O IMC de %s é: %s ' %(self.nome, imc)
