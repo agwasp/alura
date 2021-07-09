8903419a5a0ee387c4e132b00e7de5bc0f8c3a88
Alex Giannotti <alex.giannotti@questionmark.com>
2021-07-09 11:24:59 +0100

    cars and songs

--- jogos_classicos/Parte1/sketch.js
+++ jogos_classicos/Parte1/sketch.js
@@ -1,150 +1,150 @@
-  // GENERAL //
-  let xBall = 300;
-  let yBall = 200;
-  let dBall = 15; // Diametro
-  let rBall = dBall / 2; // Raio
-  let xVelocityBall = 6;
-  let yVelocityBall = 6;
-  let wPlayerRacquet = 10; // Comprimento
-  let hPlayerRacquet = 90; // Altura
-  let colision = false;
-  // PLAYER 1 //
-  let xPlayer1Racquet = 0;
-  let yPlayer1Racquet = 150;
-  // PLAYER 2 //
-  let xPlayer2Racquet = 585;
-  let yPlayer2Racquet = 150;
-  let velocityPlayer2;
-  let errorChance = 0;
-  // SCOREBOARD //
-  let player1Points = 0;
-  let player2Points = 0;
-  // GAME SONGS //
-  let points;
-  let racquet;
-  let song;
+// GENERAL //
+let xBall = 300;
+let yBall = 200;
+let dBall = 15; // Diametro
+let rBall = dBall / 2; // Raio
+let xVelocityBall = 6;
+let yVelocityBall = 6;
+let wPlayerRacquet = 10; // Comprimento
+let hPlayerRacquet = 90; // Altura
+let colision = false;
+// PLAYER 1 //
+let xPlayer1Racquet = 0;
+let yPlayer1Racquet = 150;
+// PLAYER 2 //
+let xPlayer2Racquet = 585;
+let yPlayer2Racquet = 150;
+let velocityPlayer2;
+let errorChance = 0;
+// SCOREBOARD //
+let player1Points = 0;
+let player2Points = 0;
+// GAME SONGS //
+let points;
+let racquet;
+let song;
 
-  function preload(){
-    song = loadSound("songs/song.mp3");
-    racquet = loadSound("songs/racquet.mp3");
-    points = loadSound("songs/points.mp3");
-  }
+function preload(){
+  song = loadSound("songs/song.mp3");
+  racquet = loadSound("songs/racquet.mp3");
+  points = loadSound("songs/points.mp3");
+}
 
-  function setup() {
-    createCanvas(600, 400);
-    song.loop();
-  }
+function setup() {
+  createCanvas(600, 400);
+  song.loop();
+}
+
+function draw() {
+  background(0);
   
-  function draw() {
-    background(0);
-    
-    circle(xBall, yBall, dBall);
-    moveBall();
-    colisionBorderBall();
-    scoreBoard();
-    scorePoints();
-    
-    showRacquet(xPlayer1Racquet, yPlayer1Racquet);
-    showRacquet(xPlayer2Racquet, yPlayer2Racquet);
-    
-    movePlayer1Racquet();
-    colisionPlayerRacquet(xPlayer1Racquet, yPlayer1Racquet);
-    
-    movePlayer2Racquet();
-    colisionPlayerRacquet(xPlayer2Racquet, yPlayer2Racquet);
-  }
+  circle(xBall, yBall, dBall);
+  moveBall();
+  colisionBorderBall();
+  scoreBoard();
+  scorePoints();
   
-  // GENERAL //
-  function moveBall () {
-    xBall += xVelocityBall;
-    yBall += yVelocityBall;
-  }
+  showRacquet(xPlayer1Racquet, yPlayer1Racquet);
+  showRacquet(xPlayer2Racquet, yPlayer2Racquet);
   
-  function colisionBorderBall () {
-    if (xBall + rBall > width || xBall - rBall < 0) {
-      xVelocityBall *= -1;
-    }
+  movePlayer1Racquet();
+  colisionPlayerRacquet(xPlayer1Racquet, yPlayer1Racquet);
   
-    if (yBall + rBall > height || yBall - rBall < 0) {
-      yVelocityBall *= -1;
-    }
+  movePlayer2Racquet();
+  colisionPlayerRacquet(xPlayer2Racquet, yPlayer2Racquet);
+}
+
+// GENERAL //
+function moveBall () {
+  xBall += xVelocityBall;
+  yBall += yVelocityBall;
+}
+
+function colisionBorderBall () {
+  if (xBall + rBall > width || xBall - rBall < 0) {
+    xVelocityBall *= -1;
   }
 
-  function showRacquet(x, y) {
-    rect(x, y, wPlayerRacquet, hPlayerRacquet);
+  if (yBall + rBall > height || yBall - rBall < 0) {
+    yVelocityBall *= -1;
   }
+}
 
-  function colisionPlayerRacquet(x, y) {
-    colision = collideRectCircle(x, y, wPlayerRacquet, hPlayerRacquet, xBall, yBall, rBall);
-    
-    if (colision){
-      xVelocityBall *= -1;
-      racquet.play();
-    }
-  }
+function showRacquet(x, y) {
+  rect(x, y, wPlayerRacquet, hPlayerRacquet);
+}
 
-  function scoreBoard(){
-    stroke(255);
-    textAlign(CENTER);
-    textSize(20);
-    fill(0,0,255);
-    rect(130, 10, 40, 20);
-    fill(255);
-    text(player1Points, 150, 26);
-    fill(0,0,255);
-    rect(430, 10, 40, 20);
-    fill(255);
-    text(player2Points, 450, 26);
-  }
-
-  function scorePoints(){
-    if (xBall > 590){
-      player1Points += 1;
-      points.play();
-    }
-    if (xBall < 10){
-      player2Points += 1;
-      points.play();
-    }
-  }
-
-  // PLAYER 1 //
-  function movePlayer1Racquet() {
-    if (keyIsDown(UP_ARROW)) {
-      yPlayer1Racquet -= 10;
-    }
+function colisionPlayerRacquet(x, y) {
+  colision = collideRectCircle(x, y, wPlayerRacquet, hPlayerRacquet, xBall, yBall, rBall);
   
-    if (keyIsDown(DOWN_ARROW)) {
-      yPlayer1Racquet += 10;
-    }
+  if (colision){
+    xVelocityBall *= -1;
+    racquet.play();
+  }
+}
+
+function scoreBoard(){
+  stroke(255);
+  textAlign(CENTER);
+  textSize(20);
+  fill(0,0,255);
+  rect(130, 10, 40, 20);
+  fill(255);
+  text(player1Points, 150, 26);
+  fill(0,0,255);
+  rect(430, 10, 40, 20);
+  fill(255);
+  text(player2Points, 450, 26);
+}
+
+function scorePoints(){
+  if (xBall > 590){
+    player1Points += 1;
+    points.play();
+  }
+  if (xBall < 10){
+    player2Points += 1;
+    points.play();
+  }
+}
+
+// PLAYER 1 //
+function movePlayer1Racquet() {
+  if (keyIsDown(UP_ARROW)) {
+    yPlayer1Racquet -= 10;
   }
 
-  // function colisionPlayer1Racquet() {
-  //   if (xBall - rBall < xPlayer1Racquet + wPlayerRacquet && 
-  //       yBall - rBall < yPlayer1Racquet + hPlayerRacquet && 
-  //       yBall + rBall > yPlayer1Racquet) {
-  //     xVelocityBall *= -1;
-  //   }
-  // }
-
-  // PLAYER 2 //
-  function movePlayer2Racquet() {
-    yVelocityPlayer2Racquet = yBall - yPlayer2Racquet - wPlayerRacquet / 2 - 30;
-    yPlayer2Racquet += yVelocityPlayer2Racquet + errorChance
-    calcErrorChance()
+  if (keyIsDown(DOWN_ARROW)) {
+    yPlayer1Racquet += 10;
   }
+}
 
-  function calcErrorChance() {
-    if (player2Points >= player1Points) {
-      errorChance += 1
-      if (errorChance >= 39) {
-        errorChance = 40
-      }
-    }
-    else {
-      errorChance -= 1
-      if (errorChance <= 35) {
-        errorChance = 35
-      }
+// function colisionPlayer1Racquet() {
+//   if (xBall - rBall < xPlayer1Racquet + wPlayerRacquet && 
+//       yBall - rBall < yPlayer1Racquet + hPlayerRacquet && 
+//       yBall + rBall > yPlayer1Racquet) {
+//     xVelocityBall *= -1;
+//   }
+// }
+
+// PLAYER 2 //
+function movePlayer2Racquet() {
+  yVelocityPlayer2Racquet = yBall - yPlayer2Racquet - wPlayerRacquet / 2 - 30;
+  yPlayer2Racquet += yVelocityPlayer2Racquet + errorChance
+  calcErrorChance()
+}
+
+function calcErrorChance() {
+  if (player2Points >= player1Points) {
+    errorChance += 1
+    if (errorChance >= 39) {
+      errorChance = 40
     }
   }
+  else {
+    errorChance -= 1
+    if (errorChance <= 35) {
+      errorChance = 35
+    }
+  }
+}
--- jogos_classicos/Parte2/sketch.js
+++ jogos_classicos/Parte2/sketch.js
@@ -1,7 +1,46 @@
+// GENERAL //
+let backgroundImage;
+let carGreen1;
+let carGreen2;
+let carBlack1;
+let carBlack2;
+let carYellow1;
+let carYellow2;
+// PLAYER 1 //
+let player1Image;
+// GAME SONGS //
+let colision;
+let points;
+let song;
+// SCOREBOARD //
+let player1Points = 0;
+
+function preload(){
+  colision = loadSound("songs/colision.mp3");
+  points = loadSound("songs/points.wav");
+  song = loadSound("songs/song.mp3");
+  backgroundImage = loadImage("images/highway.png");
+  player1Image = loadImage("images/player1.png");
+  carGreen1 = loadImage("images/carGreen1.png");
+  carGreen2 = loadImage("images/carGreen2.png");
+  carBlack1 = loadImage("images/carBlack1.png");
+  carBlack2 = loadImage("images/carBlack2.png");
+  carYellow1 = loadImage("images/carYellow1.png");
+  carYellow2 = loadImage("images/carYellow2.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
+  //song.loop();
 }
 
 function draw() {
-  background(220);
+  background(backgroundImage);
+  image(player1Image, 100, 366, 30, 30);
+  image(carGreen1, 450, 325, 30, 30);
+  image(carBlack1, 450, 270, 30, 30);
+  image(carYellow1, 450, 215, 30, 30);
+  image(carGreen2, 450, 155, 30, 30);
+  image(carBlack2, 450, 100, 30, 30);
+  image(carYellow2, 450, 45, 30, 30);
 }
