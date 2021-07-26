4f19e7ae4a97dda05435bf6175be84df0b49ddb2
Alex Giannotti <alex.giannotti@questionmark.com>
2021-07-26 13:15:49 +0100

    get and set

--- javascript/2_orientacao_objeto/classes/Account.js
+++ javascript/2_orientacao_objeto/classes/Account.js
@@ -1,11 +1,37 @@
+import { Branch } from "./Branch.js";
+import { Client } from "./Client.js";
+
 export class Account {
     // PUBLIC
     accountId;
 
     // Convenção da comunidade é "_clientBalance" para deixar claro que é privado, embora não seja realmente
-    // "#" é a feature para tornar uma variável privada
+    // "#" é a feature para tornar uma variável privada https://github.com/tc39/proposal-class-fields#private-fields
     // PRIVATE
+    _branch;
+    get branch() {
+        return this._branch;
+    }
+    set branch(value) {
+        if (value instanceof Branch) {
+            this._branch = value;
+        }
+    }
+
+    _client;
+    get client() {
+        return this._client;
+    }
+    set client(value) {
+        if (value instanceof Client) {
+            this._client = value;
+        }
+    }
+
     _clientBalance = 0;
+    get balance() {
+        return this._clientBalance;
+    }
 
     withdrawal (value) {
         if (this._clientBalance >= value) {
--- javascript/2_orientacao_objeto/index.js
+++ javascript/2_orientacao_objeto/index.js
@@ -2,63 +2,42 @@ import {Account} from "./classes/Account.js"
 import {Branch} from "./classes/Branch.js"
 import {Client} from "./classes/Client.js"
 
+const branch1 = new Branch();
+branch1.branchId = 1010;
+branch1.branchName = "Tucuruvi";
+branch1.branchLocation = "São Paulo";
+
+const branch2 = new Branch();
+branch2.branchId = 1015;
+branch2.branchName = "Copacabana";
+branch2.branchLocation = "Rio de Janeiro";
+
 // CLIENT 1
-const client1Branch = new Branch();
-client1Branch.branchId = 1010;
-client1Branch.branchName = "Tucuruvi";
-client1Branch.branchLocation = "São Paulo";
-client1Branch.client1Account = new Account();
-client1Branch.client1Account.accountId = 101000;
-client1Branch.client1Account.client1 = new Client();
-client1Branch.client1Account.client1.clientId = 123456;
-client1Branch.client1Account.client1.clientName = "Alex";
-client1Branch.client1Account.client1.clientLastName = "Mustermann";
-client1Branch.client1Account.deposit(500);
-client1Branch.client1Account.withdrawal(50);
+const client1 = new Client();
+client1.clientId = 123456;
+client1.clientName = "Alex";
+client1.clientLastName = "Mustermann";
+const client1Account = new Account();
+client1Account.accountId = 101000;
+client1Account.branch = branch1;
+client1Account.client = client1;
+client1Account.deposit(500);
+client1Account.withdrawal(50);
 
 // CLIENT 2
-const client2Branch = new Branch();
-client2Branch.branchId = 1015;
-client2Branch.branchName = "Mandaqui";
-client2Branch.branchLocation = "São Paulo";
-client2Branch.client2Account = new Account();
-client2Branch.client2Account.accountId = 101500;
-client2Branch.client2Account.client2 = new Client();
-client2Branch.client2Account.client2.clientId = 456789;
-client2Branch.client2Account.client2.clientName = "Max";
-client2Branch.client2Account.client2.clientLastName = "Celin";
-client2Branch.client2Account.deposit(500);
-client2Branch.client2Account.withdrawal(50);
+const client2 = new Client();
+client2.clientId = 456789;
+client2.clientName = "Maria";
+client2.clientLastName = "Celin";
+const client2Account = new Account();
+client2Account.accountId = 101500;
+client2Account.branch = branch2;
+client2Account.client = client2;
+client2Account.deposit(500);
+client2Account.withdrawal(50);
 
-client1Branch.client1Account.transfer(100, client2Branch.client2Account);
-client2Branch.client2Account.transfer(50, client1Branch.client1Account);
+// TRANSFERS
+client1Account.transfer(50, client2Account);
+client2Account.transfer(50, client1Account);
 
-console.log(client1Branch, client2Branch); //, client3Branch, client4Branch);
-
-// CLIENT 3
-// const client3Branch = new Branch();
-// client3Branch.branchId = 1020;
-// client3Branch.branchName = "Botafogo";
-// client3Branch.branchLocation = "Rio de Janeiro";
-// client3Branch.client3Account = new Account();
-// client3Branch.client3Account.accountId = 102000;
-// client3Branch.client3Account.client3 = new Client();
-// client3Branch.client3Account.client3.clientId = 789123;
-// client3Branch.client3Account.client3.clientName = "Ricardo";
-// client3Branch.client3Account.client3.clientLastName = "Koch";
-// client3Branch.client3Account.deposit(500);
-// client3Branch.client3Account.withdrawal(50);
-
-// CLIENT 4
-// const client4Branch = new Branch();
-// client4Branch.branchId = 1025;
-// client4Branch.branchName = "Copacabana";
-// client4Branch.branchLocation = "Rio de Janeiro";
-// client4Branch.client4Account = new Account();
-// client4Branch.client4Account.accountId = 102500;
-// client4Branch.client4Account.client4 = new Client();
-// client4Branch.client4Account.client4.clientId = 654987;
-// client4Branch.client4Account.client4.clientName = "Maria";
-// client4Branch.client4Account.client4.clientLastName = "Smith";
-// client4Branch.client4Account.deposit(500);
-// client4Branch.client4Account.withdrawal(50);
+console.log(client1Account, client2Account);
