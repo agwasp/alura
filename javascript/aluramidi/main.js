5d1c758624789a13b87b0f281134fcd238104513
Alex Giannotti <agwasp@users.noreply.github.com>
2023-06-07 01:17:04 +0100

    javascript beginner finshed

--- javascript/aluramidi/main.js
+++ javascript/aluramidi/main.js
@@ -1,1 +1,35 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idAudio) {
+    const elemento = document.querySelector(idAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento ou seletor não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if (event.code === "Enter" || event.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+    // console.log(contador);
+}
--- javascript/aluraphone/main.js
+++ javascript/aluraphone/main.js
@@ -1,1 +1,23 @@
-document.querySelector('input[type=tel]');
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    tecla.onclick = function () {
+        inputTel.value = inputTel.value + tecla.value;
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+    // console.log(contador);
+}
